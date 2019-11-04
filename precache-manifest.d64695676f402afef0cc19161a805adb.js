7570414a35465deb3cc70db521812169829c532a
Alexander Smurov <alexander.smurov@abz.agency>
2019-11-04 15:00:34 +0200

    made block 'font families'

---
+++ precache-manifest.d64695676f402afef0cc19161a805adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283cae371fa2c808243c375a895ec037",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fe51f6f2ff1f73a4162",
+    "url": "/static/css/2.3f58ca6a.chunk.css"
+  },
+  {
+    "revision": "8fb9e6d8dc07d911258c",
+    "url": "/static/css/main.c523ef9e.chunk.css"
+  },
+  {
+    "revision": "5fe51f6f2ff1f73a4162",
+    "url": "/static/js/2.35f9b254.chunk.js"
+  },
+  {
+    "revision": "8fb9e6d8dc07d911258c",
+    "url": "/static/js/main.e66e06c4.chunk.js"
+  },
+  {
+    "revision": "13bc1b37a803bac68174",
+    "url": "/static/js/runtime-main.8c3c2add.js"
+  }
+]);
