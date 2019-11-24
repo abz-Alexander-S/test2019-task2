6d9131c017ee131d577ae3c632bf96ff2f198ce7
Alexander Smurov <alexander.smurov@abz.agency>
2019-11-24 19:52:46 +0200

    try to up scores on pagespeed

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41ba658d8520e2fb6d1e61f416a09893.js"
+  "/precache-manifest.0d64fc0358767507b0fcd0fc545f87d4.js"
 );
 
 self.addEventListener('message', (event) => {
